b90031c0c80de5057ea9e138dfb6db8497f87842
Horris <gacheruhorris@gmail.com>
2023-09-29 11:08:52 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Mark Horris Maina  SCT211-0704/2022
+
+name = input ("What is your name?")
+print("Hello, " + name + " what numbers do you want to add?")
+ 
+int()
+num1= int(input("Enter first number:"))
+num2 = int(input ("Enter second number:"))
+
+sum = num1 + num2
+
+print (" The sum of", num1 ,"and" ,num2, "is" ,sum )
